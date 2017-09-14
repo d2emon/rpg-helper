77b47950a39b287e6222adc706e0ffacd96b7edb
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-14 10:39:59 +0300

    Commandline actions

--- src/auth/commands.py
+++ src/auth/commands.py
@@ -1,60 +1,157 @@
-from flask_script import Manager, prompt_bool
-from app import app
+from flask_script import Manager, prompt, prompt_bool, prompt_pass
+from app import app, db
+from .models import User
 
 
 manager = Manager(usage="User management")
-# from d2log import mud_logger as logger, load_logger
 # from d2lib import printfile
-# from mud.utils import getty, cls, crapup
 # from mud.talker import talker
-# from user.models import User
-# from user.login import chknolog, login  # , authenticate
+
+USERNAME_PROMPT = "By what name shall I call you?\n*\t"
+USERNAME_CONFIRM = "Did I get the name right {}? "
+PASSWORD_PROMPT = "This user already exists, what is the password? "
+NEW_USER_PASSWORD_PROMPT = "Give me a password for this user "
+TRIES = 3
+
+
+def cls():
+    print("\n" * 80)
+
+
+def crapup(text=""):
+    prompt_pass("\n{}\n\nHit Return to Continue...\n".format(text), default="")
+
+    import sys
+    sys.exit(0)
+
+
+def login(username=""):
+    '''
+    Does all the login stuff
+    The whole login system is called from this
+    '''
+    user = User.login(username)
+
+    # Get the user name
+    while not user:
+        user = input_username()
+
+    if user.is_new:
+        register(user)
+        user.qnmrq = True
+        user.ttyt = 0
+    else:
+        # Password checking
+        authenticate(user)
+    cls()
+
+    return user
+
+
+def authenticate(user):
+    '''
+    Main login code
+    '''
+    tries = TRIES
+    while tries:
+        try:
+            password = prompt_pass(PASSWORD_PROMPT)
+            return user.verify_password(password)
+        except AssertionError as e:
+            print(e)
+            tries -= 1
+        assert tries > 0, "Wrong password!"
+    return True
+
+
+def register(user):
+    '''
+    this bit registers the new user
+    '''
+    print("Creating new user...")
+
+    password = None
+    while True:
+        try:
+            user.password = prompt_pass(NEW_USER_PASSWORD_PROMPT)
+            break
+        except AssertionError as e:
+            print(e)
+    db.session.add(user)
+    db.session.commit()
+    return True
+
+
+def input_username(username="", prompt_str=USERNAME_PROMPT):
+    if not username:
+        username = prompt(prompt_str)[:15]
+
+    user = User.query.by_username(username)
+    if user:
+        return user
+
+    # If he/she doesnt exist
+    if prompt_bool(USERNAME_CONFIRM.format(username)):
+        return User(username=username.lower())
+    return user
+
+
+@manager.command
+def title():
+    '''
+    Check for all the created at stuff
+    We use stats for this which is a UN*X system call
+    '''
+    cls()
+    time_created = 0
+    time_elapsed = 0
+    print("""
+                     A B E R  M U D
+
+              By Alan Cox, Richard Acott Jim Finnis
+
+    This AberMUD was created: {}
+    {}
+    """.format(time_created, time_elapsed))
+
+
+@manager.command
+def motd():
+    '''
+    list the message of the day
+    '''
+    cls()
+    app.logger.debug("printfile(FILES['MOTD'])")
+    prompt_pass("", default="")
+    print("\n\n")
 
 
 @manager.option('-n', '--name', dest='username', default=None)
 def management(username):
     "Manage users"
-    app.logger.debug("check_host(FILES['HOST_MACHINE'])")
-    print("\n" * 4)
-    # Check if there is a no logins file active
-    app.logger.debug("chknolog()")
+    # if username is not None:
+    #     # # Now check the option entries
+    #     # # -n(name)
+    #     # # user = User.by_username(username)
+    #     # # print("USER is", user)
+    #     # # if user:
+    #     # #    # authenticate(user)
+    #     # # else:
+    #     pass
+    # else:
+    #     app.logger.debug("getty()")
+    if not username:
+        title()
 
-    user = None
-
-    if username is not None:
-        # # Now check the option entries
-        # # -n(name)
-        # # user = User.by_username(username)
-        # # print("USER is", user)
-        # # if user:
-        # #    # authenticate(user)
-        # # else:
-        app.logger.debug("user = login(username)")
-        app.logger.debug("user.qnmrq = True")
-        app.logger.debug("user.ttyt = 0")
-    else:
-        app.logger.debug("getty()")
-
-    if user is None:
-        app.logger.debug("show_title()")
-        app.logger.debug("user = login()")
-
-    # if not user.qnmrq:
-    #     show_motd()
+    user = login(username)
+    if not user.qnmrq:
+        motd()
 
     # Log entry
-    # try:
-    #     uid = os.getuid()
-    # except:
-    #     uid = '<UID>'
-    app.logger.info("Game entry by %s : UID %s", "user.username", "uid")
+    app.logger.info("Game entry by %s : UID %s", user, user.uid)
 
     # Run system
     app.logger.debug("talker(user)")
-    print("db.create_all()")
-    if prompt_bool("Are you sure? You will lose all your data!"):
-        print("db.drop_all()")
-
 
     # Exit
-    app.logger.debug("crapup(\"Bye Bye\")")
+    prompt_pass("\nBye Bye\n\nHit Return to Continue...\n", default="")
--- src/auth/models.py
+++ src/auth/models.py
@@ -1,8 +1,11 @@
 from flask_login import UserMixin
+from flask_sqlalchemy import BaseQuery
 from werkzeug.security import generate_password_hash, check_password_hash
+#
+from sqlalchemy.orm import validates
 
 
-from app import db, login_manager
+from app import app, db, login_manager
 
 
 class Role(db.Model):
@@ -21,13 +24,28 @@ class Role(db.Model):
         return '<Role: {}>'.format(self.name)
 
 
+class UserQuery(BaseQuery):
+    def by_username(self, username):
+        '''
+        Return block data for user or -1 if not exist
+        '''
+        if username is None:
+            return None
+
+        # return self.filter(Post.post_name.ilike(name)).distinct().first()
+        return self.filter_by(username=username.lower()).first()
+
+
 class User(UserMixin, db.Model):
     """
     Create an User table
     """
+    query_class = UserQuery
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(60), index=True, unique=True)
+    # username = Column(String(16), nullable=False)
+    # password = Column(String(16), nullable=False)
+    username = db.Column(db.String(60), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     # email = db.Column(db.String(60), index=True, unique=True)
     # first_name = db.Column(db.String(60), index=True)
@@ -35,11 +53,20 @@ class User(UserMixin, db.Model):
     # department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
-    role = db.relationship('Role', backref='users', lazy='dynamic')
+    # role = db.relationship('Role', backref='users', lazy='dynamic')
+    role = db.relationship('Role', backref='users')
+    # person = relationship("Person", uselist=False, back_populates="user")
+
+    nologin = False
+    __namegiv = False
+    __qnmrq = False
+    __ttyt = 0
+    __isawiz = False
 
     def __repr__(self):
-        return '<User: {}>'.format(self.username)
+        return '<User: {}>'.format(self.fullname)
 
+    # Password management
     @property
     def password(self):
         """
@@ -58,14 +85,152 @@ class User(UserMixin, db.Model):
         """
         Check if hashed password matches actual password
         """
+        # assert password == self.password, "Wrong password"
         return check_password_hash(self.password_hash, password)
 
+    # Properties
     @property
     def is_admin(self):
+        """
+        Return if user is admin
+        """
         if not self.role:
             return False
         else:
             return self.role.is_admin
+
+    @property
+    def is_new(self):
+        """
+        Return if user is new
+        """
+        return not self.id
+
+    @property
+    def uid(self):
+        # try:
+        #     uid = os.getuid()
+        # except:
+        #     uid = '<UID>'
+        return self.id
+
+    @property
+    def qnmrq(self):
+        return self.__qnmrq
+
+    @qnmrq.setter
+    def qnmrq(self, value):
+        self.__qnmrq = value
+
+    @property
+    def ttyt(self):
+        return self.__ttyt
+
+    @ttyt.setter
+    def ttyt(self, value):
+        self.__ttyt = value
+
+    @property
+    def host(self):
+        # return socket.gethostname()
+        return "Host"
+
+    @property
+    def fullname(self):
+        username = self.username.capitalize()
+        if username == "Phantom":
+            return "The {}".format(username)
+        return username
+
+    @property
+    def banned(self):
+        '''
+        Check to see if UID in banned list
+        '''
+        if not self.username:
+            return False
+        c = self.username.lower()
+
+        a = ""  # openlock(BAN_FILE,"r+");
+        if a is None:
+            return False
+        b = ''
+        for b in a:
+            if b == '\n':
+                b = ''
+            b = b.lower()
+            if b == self.host:
+                raise Exception("I'm sorry- that userid has been banned from the Game")
+        # fclose(a);
+        return False
+
+    # Other methods
+    @classmethod
+    def login(cls, username):
+        if not username:
+            return None
+
+        user = cls.query.by_username(username.lower())
+        if not user:
+            return None
+
+        # check_host(FILES['HOST_MACHINE'])
+        user.validate_host("host", "Host")
+
+        # Check if there is a no logins file active
+        user.validate_nologin()
+
+        # Check if banned first
+        if user.banned:
+            # cuserid(NULL));
+            return None
+
+        return user
+
+    @staticmethod
+    def chkname(username):
+        # assert '.' not in username, "Illegal characters in username"
+        # assert ' ' not in username, "Illegal characters in username"
+        import re
+        return re.match("^\w*$", username)
+
+    # from mud.utils import validname
+
+    # Validators
+    @validates('username')
+    def validate_username(self, key, username):
+        app.logger.debug("USERNAME %s", username)
+        assert username is not None
+        username = username.strip()
+        assert username, "Empty username"
+        assert User.chkname(username), "Illegal characters in username"
+        # assert validname(username), "Bye Bye"
+        return username
+
+    @validates('password')
+    def validate_password(self, key, password):
+        assert password is not None
+        assert password, "Empty password"
+        assert '.' not in password, "Illegal characters in password"
+        assert ' ' not in password, "Illegal characters in password"
+        return password
+
+    def validate_host(self, key, newhost):
+        '''
+        Check we are running on the correct host
+        see the notes about the use of flock();
+        and the affects of lockf();
+        '''
+        assert self.host == newhost, "AberMUD is only available on {}, not on {}".format(newhost, self.host)
+
+    def validate_nologin(self):
+        # try:
+        #     with open(CONFIG["NOLOGIN"]) as a:
+        #         s = a.read()
+        #     print(s)
+        # except:
+        #     return
+        assert self.nologin is False, "Nologin"
 
 
 @login_manager.user_loader
