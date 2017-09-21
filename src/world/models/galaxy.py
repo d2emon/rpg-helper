7af9fc82aa93e1dcac0f83d97f98779d5877e7c2
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-21 10:56:14 +0300

    Planet generator

--- src/world/models/galaxy.py
+++ src/world/models/galaxy.py
@@ -125,10 +125,12 @@ class PlanetAtmosphere():
     def __init__(self, title, exist=True):
         self.title = title
         self.exist = exist
+        
+    def __repr__(self):
+        return self.title
 
 
-class StarGenerator():
-    suns = ["sun%d" % (i + 1) for i in range(30)] + ["blue_sun%d" % (i + 30) for i in range(10)]
+class PlanetGenerator():
     margin = 5.4
     atmospheres = [
         PlanetAtmosphere("None", False),
@@ -158,10 +160,7 @@ class StarGenerator():
         "Unstable"
     ]
     map = ["Yes","No","Outdated version"]
-    drag = "on"
-    res = "on"
-    ttl = 0
-
+    
     class PlanetType():
         earth = False
         max_moons = 60
@@ -198,18 +197,76 @@ class StarGenerator():
     class GasPlanet(PlanetType):
         def __repr__(self):
             return "gasPlanet%s" % (self.image)
-        
 
-    def rndSolar(self, num_planets=0, blue_sun=False, only_blue=False):
+    @classmethod
+    def generate(cls, near=False, earth=False):
         import random
-        earthPlanets = [self.EarthPlanet(i + 1) for i in range(40)]
-        layerPlanets = [self.LayerPlanet(i + 1) for i in range(40)]
-        terplanets = [self.TerPlanet(i + 1) for i in range(40)]
-        moonPlanets = [self.MoonPlanet(i + 1) for i in range(40)]
-        gasPlanets = [self.GasPlanet(i + 1) for i in range(40)]
+        earthPlanets = [cls.EarthPlanet(i + 1) for i in range(40)]
+        layerPlanets = [cls.LayerPlanet(i + 1) for i in range(40)]
+        terplanets = [cls.TerPlanet(i + 1) for i in range(40)]
+        moonPlanets = [cls.MoonPlanet(i + 1) for i in range(40)]
+        gasPlanets = [cls.GasPlanet(i + 1) for i in range(40)]
 
         noEarthPlanets = (layerPlanets * 2) + terplanets + moonPlanets + gasPlanets
         combPlanets = earthPlanets + noEarthPlanets
+
+        rnMargin = (random.random() * cls.margin - 0.9 + 1) + 0.9
+        # if i < 6:
+        if near:
+            if not earth:
+                planet_type = random.choice(combPlanets)
+                # combPlanets.splice(ranPlanet, 1);
+                if planet_type.earth:
+                    earth = True
+            else:
+                planet_type = random.choice(noEarthPlanets)
+                # combPlanets.splice(ranPlanet, 1);
+        else:
+            planet_type = random.choice(noEarthPlanets)
+            
+        if planet_type.earth:
+            dayType = 1
+            orbitType = 1
+            atmospheres=cls.atmospheres[1:]
+        else:
+            dayType = random.randrange(2)
+            orbitType = random.randrange(2)
+            atmospheres=cls.atmospheres
+
+        if dayType == 1:
+            hours = random.randrange(43) + 8
+        else:
+            hours = random.randrange(4001) + 2000
+
+        if orbitType == 1:
+            days = ((random.random() * 2.8) + 0.2)
+        else:
+            days = ((random.random() * 191) + 10)
+            
+        return Planet(
+            planet_type=planet_type,
+            margin_left=rnMargin,
+            width=random.randrange(51) + 20,
+            environment=random.choice(cls.environment),
+            surface_map=random.choice(cls.map),
+            atmosphere=random.choice(atmospheres),
+            hours=hours,
+            days=days,
+            gravity=((random.random() * 4.8) + 0.2),
+            moons=random.randint(1, planet_type.max_moons),
+            tilt=random.random() * 180
+        )
+
+
+class StarGenerator():
+    suns = ["sun%d" % (i + 1) for i in range(30)] + ["blue_sun%d" % (i + 30) for i in range(10)]
+
+    drag = "on"
+    res = "on"
+    ttl = 0
+        
+    def rndSolar(self, num_planets=0, blue_sun=False, only_blue=False):
+        import random
 
         earth = False
         
@@ -229,52 +286,10 @@ class StarGenerator():
         sun = Sun(sun_type)
 
         for i in range(nrPlanets):
-            rnMargin = (random.random() * self.margin - 0.9 + 1) + 0.9
-            if i < 6:
-                if not earth:
-                    planet_type = random.choice(combPlanets)
-                    # combPlanets.splice(ranPlanet, 1);
-                    if planet_type.earth:
-                        earth = True
-                else:
-                    planet_type = random.choice(noEarthPlanets)
-                    # combPlanets.splice(ranPlanet, 1);
-            else:
-                planet_type = random.choice(noEarthPlanets)
-            
-            if planet_type.earth:
-                dayType = 1
-                orbitType = 1
-                atmospheres=self.atmospheres[1:]
-            else:
-                dayType = random.randrange(2)
-                orbitType = random.randrange(2)
-                atmospheres=self.atmospheres
-
-            if dayType == 1:
-                hours = random.randrange(43) + 8
-            else:
-                hours = random.randrange(4001) + 2000
-
-            if orbitType == 1:
-                days = ((random.random() * 2.8) + 0.2)
-            else:
-                days = ((random.random() * 191) + 10)
-            
-            planet = Planet(
-                planet_type=planet_type,
-                margin_left=rnMargin,
-                width=random.randrange(51) + 20,
-                environment=random.choice(self.environment),
-                surface_map=random.choice(self.map),
-                atmosphere=random.choice(atmospheres),
-                hours=hours,
-                days=days,
-                gravity=((random.random() * 4.8) + 0.2),
-                moons=random.randint(1, planet_type.max_moons),
-                tilt=random.random() * 180
-            )
+            planet = PlanetGenerator.generate(i < 6, earth)
             # curPlanet = planet.slice(0, -2)
+            if planet.planet_type.earth:
+                earth = True
             sun.planets.append(planet)
         return sun
 
