384433e4c05079e5d980b398c0eed73309a59e97
Dmitry Kutsenko <d2emonium@gmail.com>
2017-09-22 10:05:56 +0300

    Password is not nullable

--- src/auth/models.py
+++ src/auth/models.py
@@ -54,7 +54,7 @@ class User(UserMixin, db.Model):
         default="",
         info={'label': "Username"}
     )
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128), nullable=False)
     # email = db.Column(db.String(60), index=True, unique=True)
     # first_name = db.Column(db.String(60), index=True)
     # last_name = db.Column(db.String(60), index=True)
