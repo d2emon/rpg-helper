143662fb7149e7f41da068d8e9ba9f38cf89486d
Dmitry Kutsenko <d2emonium@gmail.com>
2018-04-26 16:08:22 +0300

    Confirm del

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,14 +2,7 @@
 #app
   app-header
   b-container
-    #confirm-del(class="modal fade", tabindex=-1, role="dialog")
-      .modal-dialog
-        .modal-content
-          .modal-header Вы уверены?
-          .modal-body ...
-          .modal-footer
-            button(type="button", class="btn btn-default", data-dismiss="modal") Отменить
-            a(class="btn btn-default btn-danger btn-ok") Удалить
+    confirm-del
 
     b-alert(
       v-for="message, id in messages"
---
+++ front/src/components/ConfirmDel.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  #confirm-del(class="modal fade", tabindex=-1, role="dialog")
+    .modal-dialog
+      .modal-content
+        .modal-header Вы уверены?
+        .modal-body ...
+        .modal-footer
+          button(type="button", class="btn btn-default", data-dismiss="modal") Отменить
+          a(class="btn btn-default btn-danger btn-ok") Удалить          
+</template>
+
+<script>
+export default {
+  name: 'confirm-del'
+}
+</script>
