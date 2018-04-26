d5721979657e2db62507951af0712c687b3be9a4
Dmitry Kutsenko <d2emonium@gmail.com>
2018-04-26 14:47:08 +0300

    Style

--- front/src/App.vue
+++ front/src/App.vue
@@ -105,7 +105,7 @@ export default {
   // -moz-osx-font-smoothing: grayscale;
   // text-align: center;
   // color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
 }
 
 @import "./scss/_navbar.scss";
--- front/src/components/Hello.vue
+++ front/src/components/Hello.vue
@@ -4,36 +4,10 @@
     .row
       .col-lg-12
         .intro-message
+          // img(src="../assets/logo.png")
           h1 {{ appname }}
-          h3 The best company in the world!
+          h3 {{ msg }}
           hr.intro-divider
-
-          .hello
-            img(src="../assets/logo.png")
-            h1 {{ msg }}
-            h2 Essential Links
-            ul
-              li
-                a(href="https://vuejs.org" target="_blank") Core Docs
-              li
-                a(href="https://forum.vuejs.org" target="_blank") Forum
-              li
-                a(href="https://chat.vuejs.org" target="_blank") Community Chat
-              li
-                a(href="https://twitter.com/vuejs" target="_blank") Twitter
-              br
-              li
-                a(href="http://vuejs-templates.github.io/webpack/" target="_blank") Docs for This Template
-            h2 Ecosystem
-            ul
-              li
-                a(href="http://router.vuejs.org/" target="_blank") vue-router
-              li
-                a(href="http://vuex.vuejs.org/" target="_blank") vuex
-              li
-                a(href="http://vue-loader.vuejs.org/" target="_blank") vue-loader
-              li
-                a(href="https://github.com/vuejs/awesome-vue" target="_blank") awesome-vue
 </template>
 
 <script>
@@ -42,7 +16,7 @@ export default {
   data () {
     return {
       appname: 'RPG-Helper',
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'The best company in the world!'
     }
   }
 }
@@ -50,10 +24,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
 ul {
   list-style-type: none;
   padding: 0;
@@ -62,10 +32,6 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 
 .intro-header {
