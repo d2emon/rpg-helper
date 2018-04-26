94b5bd36bf7a73404c8c36e49e8eff20df1b9cba
Dmitry Kutsenko <d2emonium@gmail.com>
2018-04-26 16:10:29 +0300

    Components

---
+++ front/src/components/index.js
@@ -0,0 +1,11 @@
+import AppHeader from './AppHeader.vue'
+import AppFooter from './AppFooter.vue'
+import ConfirmDel from './ConfirmDel.vue'
+import Messages from './Messages.vue'
+
+export {
+  AppHeader,
+  AppFooter,
+  ConfirmDel,
+  Messages
+}
